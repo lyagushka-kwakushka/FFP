be2e30e6a67b49d885adb13e9e84067dfc925e1e
lyagushka-kwakushka <kotova12873@gmail.com>
2023-11-08 16:14:57 +0300

    08.11.23 Pizza recipes!

--- app/src/main/java/com/example/pizzarecipes/MainActivity.java
+++ app/src/main/java/com/example/pizzarecipes/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
@@ -15,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<PizzaRecipeItem> pizzaRecpeItems = new ArrayList<>();
+
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza1, Utils.PIZZA_1_TITLE, Utils.PIZZA_1_DESCRIPTION, Utils.PIZZA_1_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza2, Utils.PIZZA_2_TITLE, Utils.PIZZA_2_DESCRIPTION, Utils.PIZZA_2_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza3, Utils.PIZZA_3_TITLE, Utils.PIZZA_3_DESCRIPTION, Utils.PIZZA_3_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza4, Utils.PIZZA_4_TITLE, Utils.PIZZA_4_DESCRIPTION, Utils.PIZZA_4_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza5, Utils.PIZZA_5_TITLE, Utils.PIZZA_5_DESCRIPTION, Utils.PIZZA_5_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza6, Utils.PIZZA_6_TITLE, Utils.PIZZA_6_DESCRIPTION, Utils.PIZZA_6_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza7, Utils.PIZZA_7_TITLE, Utils.PIZZA_7_DESCRIPTION, Utils.PIZZA_7_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza8, Utils.PIZZA_8_TITLE, Utils.PIZZA_8_DESCRIPTION, Utils.PIZZA_8_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza9, Utils.PIZZA_9_TITLE, Utils.PIZZA_9_DESCRIPTION, Utils.PIZZA_9_RECIPE));
+        pizzaRecpeItems.add(new PizzaRecipeItem(R.drawable.pizza10, Utils.PIZZA_10_TITLE, Utils.PIZZA_10_DESCRIPTION, Utils.PIZZA_10_RECIPE));
     }
 }
---
+++ app/src/main/java/com/example/pizzarecipes/Utils.java
@@ -0,0 +1,113 @@
+package com.example.pizzarecipes;
+
+public class Utils {
+    public static final String PIZZA_1_TITLE = "Vikaroni";
+    public static final String PIZZA_1_DESCRIPTION = "Tastya$$ pizza in da world. Your tastebuds will be grateful!";
+    public static final String PIZZA_1_RECIPE = "Heat oven to 220C" +
+            "\n" +
+            "Gather UwUika" +
+            "\n" +
+            "Burn UwUiwka to a tasty crunchy condition in the oven" +
+            "\n" +
+            "Enjoy!";
+    public static final String PIZZA_2_TITLE = "LisaBBQ";
+    public static final String PIZZA_2_DESCRIPTION = "A little bit of fur here, a little bit of fur there and wualya - it's done. Catlovers dont't watch";
+    public static final String PIZZA_2_RECIPE = "Heat oven to 220C" +
+            "\n" +
+            "Gather Lisa The Cat" +
+            "\n" +
+            "Burn Lisa The Cat to a tasty crunchy condition in the oven" +
+            "\n" +
+            "Enjoy!";
+    public static final String PIZZA_3_TITLE = "Regular pizza";
+    public static final String PIZZA_3_DESCRIPTION = "Пицца – открытый пирог в виде лепешки, покрытой начинками, в первую очередь, расплавленным сыром." +
+            "\n" +
+            "Одно из популярнейших блюд в мире, итальянское национальное блюдо. Для того, чтобы приготовить вкусную пиццу нужен хороший рецепт теста для пиццы и рецепт начинки для пиццы." +
+            "\n" +
+            "Более пятисот рецептов приготовления пиццы есть на нашем сайте, много рецептов пиццы с фото.";
+    public static final String PIZZA_3_RECIPE = "Все ингредиенты рассчитаны на 2 пиццы.\n" +
+            "Тесто для пиццы приготовлено по рецепту Джейми Оливера (ссылка на рецепт - в списке продуктов).\n" +
+            "Томатный соус у меня домашний, со сладким" +
+            "\n" +
+            "Одну часть теста помещаем на лист, покрытый пергаментной бумагой, слегка смазанной маслом, и руками распределяем тесто по кругу диаметром 30 см." +
+            "\n" +
+            "\n" +
+            "Смазываем томатным соусом, оставляя края нетронутыми." +
+            "\n" +
+            "Посыпаем сухими итальянскими травами.\n" +
+            "Сверху натираем немного сыра." +
+            "\n" +
+            "Я устал писать рецепт короче" +
+            "\n" +
+            "Помещаем пиццу в духовку, разогретую до температуры 190 градусов, на 15-20 минут, в зависимости от духовки." +
+            "\n" +
+            "Enjoy!";
+    public static final String PIZZA_4_TITLE = "Слащавый Pizza";
+    public static final String PIZZA_4_DESCRIPTION = "To cook this one you need good aim, matey. Right from Australia.";
+    public static final String PIZZA_4_RECIPE = "Heat oven to 220C" +
+            "\n" +
+            "Gather Слащавый" +
+            "\n" +
+            "Gather Pizza" +
+            "\n" +
+            "Eat'n recycle that" +
+            "\n" +
+            "Enjoy!";
+    public static final String PIZZA_5_TITLE = "Mario!";
+    public static final String PIZZA_5_DESCRIPTION = "You stinky!";
+    public static final String PIZZA_5_RECIPE = "It's a me" +
+            "\n" +
+            "Mario!" +
+            "\n" +
+            "Burn Mario to a tasty crunchy condition in the oven" +
+            "\n" +
+            "Enjoy!";
+    public static final String PIZZA_6_TITLE = "Wild pizza";
+    public static final String PIZZA_6_DESCRIPTION = "Nature can offer most gracious treats all of the time. Your tastebuds will be grateful!";
+    public static final String PIZZA_6_RECIPE = "Heat oven to 220C" +
+            "\n" +
+            "Watch as Pizza peacefully lives with it's family" +
+            "\n" +
+            "Slaughter and collect remains of Pizza and it's family, without remorse" +
+            "\n" +
+            "Burn remains to a tasty crunchy condition in the oven" +
+            "\n" +
+            "Enjoy!";
+    public static final String PIZZA_7_TITLE = "Another regular pizza in microwave";
+    public static final String PIZZA_7_DESCRIPTION = "Pizza in the microwave go brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr";
+    public static final String PIZZA_7_RECIPE = "Turn on the microwave and put pizza inside" +
+            "\n" +
+            "brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr" +
+            "\n" +
+            "Burn microwave with pizza inside to a tasty crunchy condition in the oven" +
+            "\n" +
+            "PEEEP. PEEEP. PEEEP. Enjoy!";
+    public static final String PIZZA_8_TITLE = "Freddy Fazbear circle treat";
+    public static final String PIZZA_8_DESCRIPTION = "HUR HUR HUHUR HUR HUR HUR HUR HUR HUR, HUR HUR HUR HUR, HUR HUR HUR HUR";
+    public static final String PIZZA_8_RECIPE = "Heat oven to 220C" +
+            "\n" +
+            "Gather stinky Freaddy Fazbear suit and cut it nto circles" +
+            "\n" +
+            "Burn suit circles to a tasty crunchy condition in the oven" +
+            "\n" +
+            "Enjoy!";
+    public static final String PIZZA_9_TITLE = "One HELLUVA slice";
+    public static final String PIZZA_9_DESCRIPTION = "One big ultra slice of super mega pizza";
+    public static final String PIZZA_9_RECIPE = "CLASSIFIED" +
+            "\n" +
+            "CLASSIFIED" +
+            "\n" +
+            "CLASSIFIED" +
+            "\n" +
+            "CLASSIFIED";
+    public static final String PIZZA_10_TITLE = "Box with pizza";
+    public static final String PIZZA_10_DESCRIPTION = "It's very difficult to prepare, but it's worth the troubles :). Your tastebuds will be grateful!";
+    public static final String PIZZA_10_RECIPE = "Beat some hobo people and snatch their box" +
+            "\n" +
+            "Steal some pizza or cook some yourself with our AMAZING recipes!" +
+            "\n" +
+            "Burn box with pizza inside to a tasty crunchy condition in the oven" +
+            "\n" +
+            "Enjoy!";
+
+}
